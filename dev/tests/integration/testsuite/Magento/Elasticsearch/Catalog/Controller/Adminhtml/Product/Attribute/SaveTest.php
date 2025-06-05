9bf2c06ea8c9fc52698f4e9994b5334a436649ad
magento packaging service <magento-comops@adobe.com>
2025-06-05 15:19:24 +0000

    Magento Release 2.4.5-p13

--- dev/tests/integration/testsuite/Magento/Elasticsearch/Catalog/Controller/Adminhtml/Product/Attribute/SaveTest.php
+++ dev/tests/integration/testsuite/Magento/Elasticsearch/Catalog/Controller/Adminhtml/Product/Attribute/SaveTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright 2015 Adobe
+ * All Rights Reserved.
  */
 declare(strict_types=1);
 
@@ -10,7 +10,7 @@ namespace Magento\Elasticsearch\Catalog\Controller\Adminhtml\Product\Attribute;
 use Magento\Catalog\Controller\Adminhtml\Product\Attribute\Save\AbstractSaveAttributeTest;
 
 /**
- * Test cases related to creating attribute and elasticsearch.
+ * Test cases related to creating attribute and search engine.
  */
 class SaveTest extends AbstractSaveAttributeTest
 {
